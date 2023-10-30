4f1bc8cc1e192fb357b53fdce7eaf5bea9c3f5eb
indierusty <priyaanyadav@gmail.com>
2023-10-30 11:02:44 +0530

    2nd stage - response to client

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use anyhow::Result;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+const ADDRESS: &str = "127.0.0.1:4221";
 
-    // Uncomment this block to pass the first stage
-
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+fn main() -> Result<()> {
+    let listener = TcpListener::bind(ADDRESS)?;
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        handle_client(stream?)
     }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let response = "HTTP/1.1 200 0K\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
 }
