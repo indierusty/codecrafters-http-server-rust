a7e4a4dbff88b792fb334d78100e800259bb036c
indierusty <priyaanyadav@gmail.com>
2023-10-30 12:15:45 +0530

    3rd stage Respond with 404

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // Uncomment this block to pass the first stage
-use anyhow::Result;
-use std::io::Write;
+use std::io::{Read, Result, Write};
 use std::net::{TcpListener, TcpStream};
 
 const ADDRESS: &str = "127.0.0.1:4221";
@@ -16,6 +15,15 @@ fn main() -> Result<()> {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let response = "HTTP/1.1 200 0K\r\n\r\n";
+    let mut buffer = [0; 2048];
+    stream.read(&mut buffer).unwrap();
+    let request_str = std::str::from_utf8(&buffer).unwrap();
+
+    let response = if request_str.split_whitespace().nth(1).unwrap() == "/" {
+        "HTTP/1.1 200 OK        \r\n\r\n"
+    } else {
+        "HTTP/1.1 404 NOT FOUND \r\n\r\n"
+    };
+
     stream.write(response.as_bytes()).unwrap();
 }
